bb7953c33a799c7452ce0f1a05fad117074b8941
Estefanía <estefaniacarrillosuarez@gmail.com>
2026-03-05 19:24:00 +0000

    Añadido menú prinicipal

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         prepararSistema();
 
-        System.out.println("Sistema listo");
+        mostrarMenuPrincipal();
+
     }
 
     public static void prepararSistema() {
@@ -26,8 +31,44 @@ public class Main {
             }
 
         } catch (IOException e) {
-            System.out.println("Error inicializando el sistema");
+            System.out.println("Error preparando el sistema");
         }
 
     }
+
+    public static void mostrarMenuPrincipal() {
+
+        while (true) {
+
+            System.out.println(" SISTEMA DE NOTAS ");
+            System.out.println("1. Registrarse");
+            System.out.println("2. Iniciar sesión");
+            System.out.println("0. Salir");
+            System.out.print("Elige una opción: ");
+
+            String opcion = sc.nextLine();
+
+            switch (opcion) {
+
+                case "1":
+                    System.out.println("Registro de usuario (pendiente)");
+                    break;
+
+                case "2":
+                    System.out.println("Inicio de sesión (pendiente)");
+                    break;
+
+                case "0":
+                    System.out.println("Saliendo del programa...");
+                    return;
+
+                default:
+                    System.out.println("Opción no válida");
+
+            }
+
+        }
+
+    }
+
 }
